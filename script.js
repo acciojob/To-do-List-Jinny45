477e694ff24c23f260fa208670db2888f911f4fe
Jinashree Gane <113277171+Jinny45@users.noreply.github.com>
2023-03-01 19:23:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
 
+JavaScript
+var todovalue = document.getElementById("newTodoInput");
+var btn = document.getElementById("addTodoBtn");
+var ol = document.getElementById("todoList");
+
+function xys()
+{
+	if(todovalue.value)
+	{
+		var abc = document.createElement('li');
+		abc.textContent = todovalue.value;
+		ol.appendChild(abc);
+		todovalue.value = "";
+	}
+}
+btn.addEventListener("click" , xys);
